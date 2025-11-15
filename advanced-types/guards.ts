5eac28e1f54560d556acd6882879de43dc128033
faridreaming <97384053+faridreaming@users.noreply.github.com>
2025-11-15 20:47:42 +0700

    feat: learn about type guard via instanceof operator

--- advanced-types/guards.ts
+++ advanced-types/guards.ts
@@ -21,3 +21,32 @@ function loadData(source: Source) {
 }
 
 loadData(dbSource)
+
+class User {
+  constructor(public name: string) {}
+
+  join() {
+    // ...
+  }
+}
+
+class Admin {
+  constructor(permissions: string[]) {}
+
+  scan() {
+    // ...
+  }
+}
+
+const user = new User('Max')
+const admin = new Admin(['ban', 'restore'])
+
+type Entity = User | Admin
+
+function init(entity: Entity) {
+  if (entity instanceof User) {
+    entity.join()
+    return
+  }
+  entity.scan()
+}
