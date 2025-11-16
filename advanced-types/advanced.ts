ae697e23621510d9ae376127f3fec7446354029c
faridreaming <97384053+faridreaming@users.noreply.github.com>
2025-11-16 15:51:25 +0700

    learn about the `satisfies` keyword

--- advanced-types/advanced.ts
+++ advanced-types/advanced.ts
@@ -14,3 +14,10 @@ let nama = 'farid' as const
 let roles: readonly ['admin', 'user', 'guest'] = ['admin', 'user', 'guest']
 // roles.push('pumbah')
 let firstRole = roles[0]
+
+const dataEntries = {
+  entry1: 12,
+  entry2: -12,
+} as const satisfies Record<string, number>
+
+let entry1 = dataEntries.entry1
