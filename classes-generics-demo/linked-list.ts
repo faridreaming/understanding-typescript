9e68add64853dc2173710faf3f5bd706930fa95a
faridreaming <97384053+faridreaming@users.noreply.github.com>
2025-11-16 23:28:59 +0700

    classes-generics-demo: creating generic type for the classes and adding add method

---
+++ classes-generics-demo/linked-list.ts
@@ -0,0 +1,27 @@
+class ListNode<T> {
+  next?: ListNode<T>
+
+  constructor(public value: T) {}
+}
+
+class LinkedList<T> {
+  private root?: ListNode<T>
+  private length = 0
+
+  add(value: T) {
+    const node = new ListNode(value)
+    if (!this.root) {
+      this.root = node
+    } else {
+      let current = node
+      while (current.next) {
+        current = current.next
+      }
+      current.next = node
+    }
+    this.length++
+  }
+}
+
+const numberList = new LinkedList<number>()
+const nameList = new LinkedList<string>()
