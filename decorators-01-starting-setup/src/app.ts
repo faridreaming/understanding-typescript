076f32a70792b8bb07fa16d981265a190a9534ba
faridreaming <97384053+faridreaming@users.noreply.github.com>
2025-11-23 08:55:24 +0700

    experimental-decorators: building more useful decorators

--- decorators-01-starting-setup/src/app.ts
+++ decorators-01-starting-setup/src/app.ts
@@ -5,7 +5,18 @@ function Logger(logString: string) {
   }
 }
 
-@Logger('LOGGING - PERSON')
+function WithTemplate(template: string, hookId: string) {
+  return function (constructor: any) {
+    const hookEl = document.getElementById(hookId)
+    const person = new constructor()
+    if (hookEl) {
+      hookEl.innerHTML = template
+      hookEl.querySelector('h1')!.textContent = person.name
+    }
+  }
+}
+
+@WithTemplate('<h1>My Person Object 🥺</h1>', 'app')
 class Person {
   name = 'Farid'
 
