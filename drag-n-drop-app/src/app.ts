70fea652e9faf798ab24abd0b444a422773031a2
faridreaming <97384053+faridreaming@users.noreply.github.com>
2025-11-30 20:19:29 +0700

    feat: drag events and reflecting the current state in the ui

--- drag-n-drop-app/src/app.ts
+++ drag-n-drop-app/src/app.ts
@@ -215,7 +215,10 @@ class ProjectItem
 }
 
 // ProjectList Class (parent)
-class ProjectList extends Component<HTMLDivElement, HTMLElement> {
+class ProjectList
+  extends Component<HTMLDivElement, HTMLElement>
+  implements DragTarget
+{
   assignedProjects: Project[]
 
   constructor(private type: 'active' | 'finished') {
@@ -226,7 +229,25 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     this.configure()
   }
 
+  @autobind
+  dragOverHandler(_: DragEvent) {
+    const listEl = this.element.querySelector('ul')!
+    listEl.classList.add('droppable')
+  }
+
+  dropHandler(_: DragEvent) {}
+
+  @autobind
+  dragLeaveHandler(_: DragEvent) {
+    const listEl = this.element.querySelector('ul')!
+    listEl.classList.remove('droppable')
+  }
+
   configure() {
+    this.element.addEventListener('dragover', this.dragOverHandler)
+    this.element.addEventListener('drop', this.dropHandler)
+    this.element.addEventListener('dragleave', this.dragLeaveHandler)
+
     projectState.addListener((projects: Project[]) => {
       const relevantProjects = projects.filter((prj) => {
         if (this.type === 'active') {
