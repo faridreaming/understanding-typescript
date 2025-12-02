d79d80797964eccdf9503f6aecfc958d03c86506
faridreaming <97384053+faridreaming@users.noreply.github.com>
2025-12-02 16:48:31 +0700

    modules-and-namespaces: a problem with namespaces imports

--- modules-and-namespaces/src/app.ts
+++ modules-and-namespaces/src/app.ts
@@ -1,10 +1,5 @@
-/// <reference path="models/drag-and-drop.ts" />
-/// <reference path="models/project.ts" />
 /// <reference path="state/project.ts" />
-/// <reference path="util/validation.ts" />
-/// <reference path="decorators/autobind.ts" />
 /// <reference path="components/project-input.ts" />
-/// <reference path="components/project-item.ts" />
 /// <reference path="components/project-list.ts" />
 
 namespace App {
