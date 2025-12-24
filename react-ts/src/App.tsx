fc8d36bf83662b6f539435b2ad4d712f832aacaa
faridreaming <97384053+faridreaming@users.noreply.github.com>
2025-12-24 10:09:26 +0700

    section-19: passing functions as props

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,28 +1,33 @@
 import Header from './components/Header'
 import goalsImg from './assets/goals.jpg'
 import CourseGoals from './components/CourseGoals'
+import { useState } from 'react'
 
 function App() {
+  const [goals, setGoals] = useState([
+    {
+      id: 1,
+      title: 'Financial freedom',
+      description: 'Have financial freedom',
+    },
+    {
+      id: 2,
+      title: 'Being more discipline',
+      description: 'Develop better habits and consistency',
+    },
+  ])
+
+  function handleDeleteGoal(id: number) {
+    setGoals((prevGoals) => prevGoals.filter((g) => g.id !== id))
+  }
+
   return (
     <>
       <main>
         <Header image={{ src: goalsImg, alt: 'My goals' }}>
           <h1>My Goals</h1>
         </Header>
-        <CourseGoals
-          goals={[
-        {
-          id: 1,
-          title: 'Financial freedom',
-          description: 'Have financial freedom',
-        },
-        {
-          id: 2,
-          title: 'Being more discipline',
-          description: 'Develop better habits and consistency',
-        },
-          ]}
-        />
+        <CourseGoals goals={goals} onDelete={handleDeleteGoal} />
       </main>
     </>
   )
