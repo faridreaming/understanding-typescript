2d3a4b449231eefe9a620b2afcda9204adb5bc7e
faridreaming <97384053+faridreaming@users.noreply.github.com>
2025-12-24 10:00:35 +0700

    section-19: adding another components with props

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import goalsImg from './assets/goals.jpg'
+import CourseGoals from './components/CourseGoals'
 
 function App() {
   return (
@@ -8,6 +9,20 @@ function App() {
         <Header image={{ src: goalsImg, alt: 'My goals' }}>
           <h1>My Goals</h1>
         </Header>
+        <CourseGoals
+          goals={[
+        {
+          id: 1,
+          title: 'Financial freedom',
+          description: 'Have financial freedom',
+        },
+        {
+          id: 2,
+          title: 'Being more discipline',
+          description: 'Develop better habits and consistency',
+        },
+          ]}
+        />
       </main>
     </>
   )
---
+++ react-ts/src/components/CourseGoals.tsx
@@ -0,0 +1,32 @@
+import type { FC } from 'react'
+
+type Goal = {
+  id: number
+  title: string
+  description: string
+}
+
+interface CourseGoalsProps {
+  goals: Goal[]
+}
+
+// export default function CourseGoals({ goals }: CourseGoalsProps) {
+const CourseGoals: FC<CourseGoalsProps> = ({ goals }) => {
+  return (
+    <ul>
+      {goals.map((goal) => (
+        <li key={goal.id}>
+          <article>
+            <div>
+              <h2>{goal.title}</h2>
+              <p>{goal.description}</p>
+            </div>
+            <button>Delete</button>
+          </article>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default CourseGoals
